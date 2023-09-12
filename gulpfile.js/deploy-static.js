b36b2a8c99f928c9e07c66a80d4449c2b4e8a6b0
Anant Agrawal <anant-k-agrawal@users.noreply.github.com>
2023-09-12 19:48:15 +0530

    Update deploy-static.js

--- gulpfile.js/deploy-static.js
+++ gulpfile.js/deploy-static.js
@@ -42,7 +42,9 @@ async function staticDeploy() {
     console.log(`attempting to deploy ${SITE.DIR}`);
 
     await sh(`npx browserslist@latest --update-db`);
+    await sh(`pwd`);
     await sh(`tree -a -d -f /home/runner/work/amp.dev/amp.dev/`);
+    await sh(`netlify link`);
     await sh(
       `npx netlify deploy --prod --auth ${NETLIFY_DEPLOY_TOKEN} --site ${SITE.ID}`,
       {
