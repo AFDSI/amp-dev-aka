96e42404e6ec661231ff9901a728b55d750664dd
Anant Agrawal <anant-k-agrawal@users.noreply.github.com>
2023-09-20 14:58:27 +0530

    Update deploy-static.js

--- gulpfile.js/deploy-static.js
+++ gulpfile.js/deploy-static.js
@@ -49,7 +49,7 @@ async function staticDeploy() {
     await sh(`pwd`);
     // await sh(`echo ${SITE.NAME} ${SITE.ID} ${SITE.PAGES_DEST} ${NETLIFY_DEPLOY_TOKEN}`);
     await sh(
-      `npx netlify deploy --prod --auth ${NETLIFY_DEPLOY_TOKEN} --site ${SITE.ID}`,
+      `npx netlify@16.4.1 deploy --prod --auth ${NETLIFY_DEPLOY_TOKEN} --site ${SITE.ID}`,
       {
         workingDir: SITE.DIR,
       }
