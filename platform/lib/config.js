155af857a751e4b4d24ddb47b18d8eb29e713f3e
jay gray <jaygray0919@gmail.com>
2024-02-08 12:01:03 -0600

    removed trailing ocmma

--- platform/lib/config.js
+++ platform/lib/config.js
@@ -34,7 +34,7 @@ const ENV_LOCAL = 'local';
 
 const DEFAULT_LOCALE = 'en';
 
-const AVAILABLE_LOCALES = ['en','es','fr',];
+const AVAILABLE_LOCALES = ['en','es','fr'];
 
 class Config {
   constructor(environment = ENV_DEV) {
