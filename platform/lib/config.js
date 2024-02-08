913425bd4957da0a4c50cd4fba81d5f5debb6449
jay gray <jaygray0919@gmail.com>
2024-02-08 11:46:28 -0600

    updated config.js
    removed comments
    simple 3 item list

--- platform/lib/config.js
+++ platform/lib/config.js
@@ -36,20 +36,8 @@ const DEFAULT_LOCALE = 'en';
 
 const AVAILABLE_LOCALES = [
   'en',
-//  'de',
   'fr',
-//  'ar',
   'es',
-//  'it',
-//  'id',
-//  'ja',
-//  'ko',
-//  'pt_BR',
-//  'ru',
-//  'tr',
-//  'zh_CN',
-//  'pl',
-//  'vi',
 ];
 
 class Config {
