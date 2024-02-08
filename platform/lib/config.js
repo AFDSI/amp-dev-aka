c14e0cf5d5b9a0cdb797fcb403038f26c16026d4
jay gray <jaygray0919@gmail.com>
2024-02-08 11:37:10 -0600

    comment all but en and es

--- platform/lib/config.js
+++ platform/lib/config.js
@@ -36,20 +36,20 @@ const DEFAULT_LOCALE = 'en';
 
 const AVAILABLE_LOCALES = [
   'en',
-  'de',
-  'fr',
-  'ar',
+//  'de',
+//  'fr',
+//  'ar',
   'es',
-  'it',
-  'id',
-  'ja',
-  'ko',
-  'pt_BR',
-  'ru',
-  'tr',
-  'zh_CN',
-  'pl',
-  'vi',
+//  'it',
+//  'id',
+//  'ja',
+//  'ko',
+//  'pt_BR',
+//  'ru',
+//  'tr',
+//  'zh_CN',
+//  'pl',
+//  'vi',
 ];
 
 class Config {
